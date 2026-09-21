bd9974cb6a20aa31cc489264cbdb60365b07cc45
Giovanna Marques <giovannamrodrigues2006@gmail.com>
2026-09-20 21:05:45 -0300

    feat(#T1.3) criar endpoint GET /datasets para popular o seletor no upload

---
+++ georuraldatahub/src/test/java/com/visiona/georuraldatahub/GeoruraldatahubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visiona.georuraldatahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoruraldatahubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
