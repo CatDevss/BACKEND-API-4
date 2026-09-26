d283b4c8985d41b0b721ece20d652828cd51b633
pedroquirino <pedrohpquirino@gmail.com>
2026-09-26 11:52:30 -0300

    feat(T2.3): adiciona upload de arquivo na zona bruta do bucket

--- georuraldatahub/src/main/java/catdevs/georuraldatahub/service/BucketService.java
+++ georuraldatahub/src/main/java/catdevs/georuraldatahub/service/BucketService.java
@@ -3,9 +3,12 @@ package catdevs.georuraldatahub.service;
 import com.oracle.bmc.objectstorage.ObjectStorageClient;
 import com.oracle.bmc.objectstorage.model.ObjectSummary;
 import com.oracle.bmc.objectstorage.requests.ListObjectsRequest;
+import com.oracle.bmc.objectstorage.requests.PutObjectRequest;
 import catdevs.georuraldatahub.config.ObjectStorageProperties;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -39,5 +42,24 @@ public class BucketService {
                 .stream()
                 .map(ObjectSummary::getName)
                 .toList();
+    }
+
+    /**
+     * Envia o arquivo para a zona bruta do bucket, no formato
+     * "bruta/{datasetId}/{hash}_{nomeOriginal}", e retorna o nome do objeto salvo.
+     */
+    public String uploadToRawZone(MultipartFile file, Long datasetId, String hash) throws IOException {
+        String objectName = "bruta/" + datasetId + "/" + hash + "_" + file.getOriginalFilename();
+
+        PutObjectRequest request = PutObjectRequest.builder()
+                .namespaceName(props.namespace())
+                .bucketName(props.bucketName())
+                .objectName(objectName)
+                .putObjectBody(file.getInputStream())
+                .contentLength(file.getSize())
+                .build();
+
+        client.putObject(request);
+        return objectName;
     }
 }
